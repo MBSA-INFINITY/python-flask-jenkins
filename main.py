b62dd2c4f59f9313b11ff02977a83f7792bec93d
M.B.SAI ADITYA <85332648+MBSA-INFINITY@users.noreply.github.com>
2024-06-26 04:03:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from flask import Flask
 
-app = Flask(__name__
+app = Flask(__name__)
 
 @app.route("/")
 def start():
     return "MBSA's Flask Server Successfully started."
 
-if __name__ == '__main__':
-    app.run()
+
